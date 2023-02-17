1d539fd76ff45941238ccb580cd4231ccb7db7cb
Maieza Borges <maieza.borges@gmail.com>
2023-02-16 21:15:16 -0400

    fixed bug

--- app/Http/Requests/ProductUpdate.php
+++ app/Http/Requests/ProductUpdate.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProductUpdate extends FormRequest
 {
-    public function authorize()
-    {
-        return false;
-    }
-
     public function rules()
     {
         return [
